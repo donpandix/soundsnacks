d024ada90bd7e9063d51b30addb3f6c73ad26f2b
Cesar Gonzalez M <cesar@cesarg.cl>
2026-01-10 10:21:17 -0300

    Initial Commit

---
+++ SoundSnacks/SoundSnacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundSnacksApp.swift
+//  SoundSnacks
+//
+//  Created by Cesar Gonzalez Molina on 10-01-26.
+//
+
+import SwiftUI
+
+@main
+struct SoundSnacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
